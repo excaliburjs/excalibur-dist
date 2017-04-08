4da4e400ec1a5513fd29d693b7def4f6502cc6d5
Travis-CI <travis@excaliburjs.com>
2017-04-08 01:18:05 +0000

    :shipit: Built excaliburjs/Excalibur@129febf on branch HEAD

--- dist/excalibur.js
+++ dist/excalibur.js
@@ -9593,6 +9593,224 @@ define("Math/Random", ["require", "exports"], function (require, exports) {
     }());
     exports.Random = Random;
 });
+define("Math/PerlinNoise", ["require", "exports", "Math/Random", "Drawing/Color", "Util/Util"], function (require, exports, Random_1, Color_18, Util) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function _lerp(time, a, b) {
+        return a + time * (b - a);
+    }
+    function _fade(t) {
+        return t * t * t * (t * (t * 6 - 15) + 10);
+    }
+    /**
+     * Generates perlin noise based on the 2002 Siggraph paper http://mrl.nyu.edu/~perlin/noise/
+     * Also https://flafla2.github.io/2014/08/09/perlinnoise.html
+     */
+    var PerlinGenerator = (function () {
+        function PerlinGenerator(options) {
+            this._perm = [151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225,
+                140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26,
+                197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136,
+                171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60,
+                211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80,
+                73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198,
+                173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206,
+                59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70,
+                221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178,
+                185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81,
+                51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115,
+                121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195,
+                78, 66, 215, 61, 156, 180];
+            this._p = new Uint8Array(512);
+            this._defaultPerlinOptions = {
+                octaves: 1,
+                frequency: 1,
+                amplitude: 1,
+                persistance: .5
+            };
+            options = Util.extend({}, this._defaultPerlinOptions, options);
+            this.persistance = options.persistance;
+            this.amplitude = options.amplitude;
+            this.frequency = options.frequency;
+            this.octaves = options.octaves;
+            if (options.seed) {
+                this._random = new Random_1.Random(options.seed);
+            }
+            else {
+                this._random = new Random_1.Random();
+            }
+            this._perm = this._random.shuffle(this._perm);
+            for (var i = 0; i < 512; i++) {
+                this._p[i] = this._perm[i % 256] & 0xFF;
+            }
+        }
+        PerlinGenerator.prototype.noise = function () {
+            var amp = this.amplitude;
+            var freq = this.frequency;
+            var total = 0;
+            var maxValue = 0;
+            for (var i = 0; i < this.octaves; i++) {
+                switch (arguments.length) {
+                    case 1:
+                        total += this._noise1d(arguments[0] * freq) * amp;
+                        break;
+                    case 2:
+                        total += this._noise2d(arguments[0] * freq, arguments[1] * freq) * amp;
+                        break;
+                    case 3:
+                        total += this._noise3d(arguments[0] * freq, arguments[1] * freq, arguments[2] * freq) * amp;
+                        break;
+                    /* istanbul ignore next */
+                    default: throw new Error('Invalid arguments for perlin noise');
+                }
+                maxValue += amp;
+                amp *= this.persistance;
+                freq *= 2;
+            }
+            return total / maxValue;
+        };
+        /**
+         * Generates a list starting at 0 and ending at 1 of contious perlin noise, by default the step is 1/length;
+         *
+         */
+        PerlinGenerator.prototype.sequence = function (length, step) {
+            if (!step) {
+                step = 1 / length;
+            }
+            var array = new Array(length);
+            for (var i = 0; i < length; i++) {
+                array[i] = this.noise(i * step);
+            }
+            return array;
+        };
+        /**
+         * Generates a 2D grid of perlin noise given a step value packed into a 1D array i = (x + y*width),
+         * by default the step will 1/(min(dimension))
+         */
+        PerlinGenerator.prototype.grid = function (width, height, step) {
+            if (!step) {
+                step = 1 / (Math.min(width, height));
+            }
+            var array = new Array(width * height);
+            for (var y = 0; y < height; y++) {
+                for (var x = 0; x < width; x++) {
+                    array[x + y * width] = this.noise(x * step, y * step);
+                }
+            }
+            return array;
+        };
+        PerlinGenerator.prototype._gradient3d = function (hash, x, y, z) {
+            var h = hash & 0xF;
+            var u = h < 8 ? x : y;
+            var v = h < 4 ? y : ((h === 12 || h === 14) ? x : z);
+            return ((h & 1) === 0 ? u : -u) + ((h & 2) === 0 ? v : -v);
+        };
+        PerlinGenerator.prototype._gradient2d = function (hash, x, y) {
+            var value = (hash & 1) === 0 ? x : y;
+            return (hash & 2) === 0 ? -value : value;
+        };
+        PerlinGenerator.prototype._gradient1d = function (hash, x) {
+            return (hash & 1) === 0 ? -x : x;
+        };
+        PerlinGenerator.prototype._noise1d = function (x) {
+            var intX = Math.floor(x) & 0xFF; // force 0-255 integers to lookup in permutation
+            x -= Math.floor(x);
+            var fadeX = _fade(x);
+            return (_lerp(fadeX, this._gradient1d(this._p[intX], x), this._gradient1d(this._p[intX + 1], x - 1)) + 1) / 2;
+        };
+        PerlinGenerator.prototype._noise2d = function (x, y) {
+            var intX = Math.floor(x) & 0xFF;
+            var intY = Math.floor(y) & 0xFF;
+            x -= Math.floor(x);
+            y -= Math.floor(y);
+            var fadeX = _fade(x);
+            var fadeY = _fade(y);
+            var a = this._p[intX] + intY;
+            var b = this._p[intX + 1] + intY;
+            return (_lerp(fadeY, _lerp(fadeX, this._gradient2d(this._p[a], x, y), this._gradient2d(this._p[b], x - 1, y)), _lerp(fadeX, this._gradient2d(this._p[a + 1], x, y - 1), this._gradient2d(this._p[b + 1], x - 1, y - 1))) + 1) / 2;
+        };
+        PerlinGenerator.prototype._noise3d = function (x, y, z) {
+            var intX = Math.floor(x) & 0xFF;
+            var intY = Math.floor(y) & 0xFF;
+            var intZ = Math.floor(z) & 0xFF;
+            x -= Math.floor(x);
+            y -= Math.floor(y);
+            z -= Math.floor(z);
+            var fadeX = _fade(x);
+            var fadeY = _fade(y);
+            var fadeZ = _fade(z);
+            var a = this._p[intX] + intY;
+            var b = this._p[intX + 1] + intY;
+            var aa = this._p[a] + intZ;
+            var ba = this._p[b] + intZ;
+            var ab = this._p[a + 1] + intZ;
+            var bb = this._p[b + 1] + intZ;
+            return (_lerp(fadeZ, _lerp(fadeY, _lerp(fadeX, this._gradient3d(this._p[aa], x, y, z), this._gradient3d(this._p[ba], x - 1, y, z)), _lerp(fadeX, this._gradient3d(this._p[ab], x, y - 1, z), this._gradient3d(this._p[bb], x - 1, y - 1, z))), _lerp(fadeY, _lerp(fadeX, this._gradient3d(this._p[aa + 1], x, y, z - 1), this._gradient3d(this._p[ba + 1], x - 1, y, z - 1)), _lerp(fadeX, this._gradient3d(this._p[ab + 1], x, y - 1, z - 1), this._gradient3d(this._p[bb + 1], x - 1, y - 1, z - 1)))) + 1) / 2;
+        };
+        return PerlinGenerator;
+    }());
+    exports.PerlinGenerator = PerlinGenerator;
+    /**
+     * A helper to draw 2D perlin maps given a perlin generator and a function
+     */
+    var PerlinDrawer2D = (function () {
+        /**
+         * @param generator - An existing perlin generator
+         * @param colorFcn - A color function that takes a value between [0, 255] derived from the perlin generator, and returns a color
+         */
+        function PerlinDrawer2D(generator, colorFcn) {
+            this.generator = generator;
+            this.colorFcn = colorFcn;
+            if (!colorFcn) {
+                this.colorFcn = function (val) { return val < 125 ? Color_18.Color.Black : Color_18.Color.White; };
+            }
+        }
+        /**
+         * Returns an image of 2D perlin noise
+         */
+        PerlinDrawer2D.prototype.image = function (width, height) {
+            var image = document.createElement('img');
+            var canvas = document.createElement('canvas');
+            canvas.width = width;
+            canvas.height = height;
+            var ctx = canvas.getContext('2d');
+            this.draw(ctx, 0, 0, width, height);
+            image.src = canvas.toDataURL();
+            return image;
+        };
+        /**
+         * This draws a 2D perlin grid on a canvas context, not recommended to be called every frame due to performance
+         */
+        PerlinDrawer2D.prototype.draw = function (ctx, x, y, width, height) {
+            var grid = this.generator.grid(width, height);
+            var imageData = ctx.getImageData(x, y, width, height);
+            for (var j = 0; j < height; j++) {
+                for (var i = 0; i < width; i++) {
+                    var val = grid[i + width * j];
+                    var c = Math.floor(val * 255) & 0xff;
+                    var pixel = (i + j * imageData.width) * 4;
+                    var color = this.colorFcn(c);
+                    imageData.data[pixel] = color.r;
+                    imageData.data[pixel + 1] = color.g;
+                    imageData.data[pixel + 2] = color.b;
+                    imageData.data[pixel + 3] = Math.floor(color.a * 255);
+                }
+            }
+            ctx.putImageData(imageData, x, y);
+        };
+        return PerlinDrawer2D;
+    }());
+    exports.PerlinDrawer2D = PerlinDrawer2D;
+});
+define("Math/Index", ["require", "exports", "Math/PerlinNoise", "Math/Random"], function (require, exports, PerlinNoise_1, Random_2) {
+    "use strict";
+    function __export(m) {
+        for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+    }
+    Object.defineProperty(exports, "__esModule", { value: true });
+    __export(PerlinNoise_1);
+    __export(Random_2);
+});
 define("PostProcessing/IPostProcessor", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -11161,7 +11379,7 @@ define("Util/SortedList", ["require", "exports"], function (require, exports) {
     }());
     exports.MockedElement = MockedElement;
 });
-define("Index", ["require", "exports", "Actor", "Algebra", "Camera", "Class", "Debug", "Engine", "EventDispatcher", "Events", "Group", "Label", "Loader", "Particles", "Physics", "Promises", "Scene", "TileMap", "Timer", "Trigger", "UIActor", "Actions/Index", "Collision/Index", "Drawing/Index", "Math/Random", "PostProcessing/Index", "Resources/Index", "Events", "Input/Index", "Traits/Index", "Util/Index", "Util/Decorators", "Util/Detector", "Util/CullingBox", "Util/EasingFunctions", "Util/Log", "Util/SortedList"], function (require, exports, Actor_10, Algebra_20, Camera_1, Class_8, Debug_1, Engine_1, EventDispatcher_2, Events_8, Group_1, Label_2, Loader_1, Particles_1, Physics_11, Promises_7, Scene_1, TileMap_1, Timer_1, Trigger_1, UIActor_2, Index_1, Index_2, Index_3, Random_1, Index_4, Index_5, events, input, traits, util, Decorators_2, Detector_1, CullingBox_2, EasingFunctions_3, Log_13, SortedList_1) {
+define("Index", ["require", "exports", "Actor", "Algebra", "Camera", "Class", "Debug", "Engine", "EventDispatcher", "Events", "Group", "Label", "Loader", "Particles", "Physics", "Promises", "Scene", "TileMap", "Timer", "Trigger", "UIActor", "Actions/Index", "Collision/Index", "Drawing/Index", "Math/Index", "PostProcessing/Index", "Resources/Index", "Events", "Input/Index", "Traits/Index", "Util/Index", "Util/Decorators", "Util/Detector", "Util/CullingBox", "Util/EasingFunctions", "Util/Log", "Util/SortedList"], function (require, exports, Actor_10, Algebra_20, Camera_1, Class_8, Debug_1, Engine_1, EventDispatcher_2, Events_8, Group_1, Label_2, Loader_1, Particles_1, Physics_11, Promises_7, Scene_1, TileMap_1, Timer_1, Trigger_1, UIActor_2, Index_1, Index_2, Index_3, Index_4, Index_5, Index_6, events, input, traits, util, Decorators_2, Detector_1, CullingBox_2, EasingFunctions_3, Log_13, SortedList_1) {
     "use strict";
     function __export(m) {
         for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
@@ -11195,9 +11413,9 @@ define("Index", ["require", "exports", "Actor", "Algebra", "Camera", "Class", "D
     __export(Index_1);
     __export(Index_2);
     __export(Index_3);
-    __export(Random_1);
     __export(Index_4);
     __export(Index_5);
+    __export(Index_6);
     exports.Events = events;
     exports.Input = input;
     exports.Traits = traits;
@@ -11209,7 +11427,7 @@ define("Index", ["require", "exports", "Actor", "Algebra", "Camera", "Class", "D
     __export(Log_13);
     __export(SortedList_1);
 });
-define("Engine", ["require", "exports", "Index", "Promises", "Algebra", "UIActor", "Actor", "Timer", "TileMap", "Loader", "Util/Detector", "Events", "Util/Log", "Drawing/Color", "Scene", "Debug", "Class", "Input/Index", "Util/Decorators", "Util/Util", "Collision/BoundingBox"], function (require, exports, Index_6, Promises_8, Algebra_21, UIActor_3, Actor_11, Timer_2, TileMap_2, Loader_2, Detector_2, Events_9, Log_14, Color_18, Scene_2, Debug_2, Class_9, Input, Decorators_3, Util, BoundingBox_7) {
+define("Engine", ["require", "exports", "Index", "Promises", "Algebra", "UIActor", "Actor", "Timer", "TileMap", "Loader", "Util/Detector", "Events", "Util/Log", "Drawing/Color", "Scene", "Debug", "Class", "Input/Index", "Util/Decorators", "Util/Util", "Collision/BoundingBox"], function (require, exports, Index_7, Promises_8, Algebra_21, UIActor_3, Actor_11, Timer_2, TileMap_2, Loader_2, Detector_2, Events_9, Log_14, Color_19, Scene_2, Debug_2, Class_9, Input, Decorators_3, Util, BoundingBox_7) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /**
@@ -11295,11 +11513,11 @@ define("Engine", ["require", "exports", "Index", "Promises", "Algebra", "UIActor
              * Indicates whether the engine should draw with debug information
              */
             _this.isDebug = false;
-            _this.debugColor = new Color_18.Color(255, 255, 255);
+            _this.debugColor = new Color_19.Color(255, 255, 255);
             /**
              * Sets the background color for the engine.
              */
-            _this.backgroundColor = new Color_18.Color(0, 0, 100);
+            _this.backgroundColor = new Color_19.Color(0, 0, 100);
             /**
              * The action to take when a fatal exception is thrown
              */
@@ -11332,7 +11550,7 @@ define("Engine", ["require", "exports", "Index", "Promises", "Algebra", "UIActor
             }
             // Use native console API for color fun
             if (console.log && !options.suppressConsoleBootMessage) {
-                console.log("%cPowered by Excalibur.js (v" + Index_6.EX_VERSION + ")", 'background: #176BAA; color: white; border-radius: 5px; padding: 15px; font-size: 1.5em; line-height: 80px;');
+                console.log("%cPowered by Excalibur.js (v" + Index_7.EX_VERSION + ")", 'background: #176BAA; color: white; border-radius: 5px; padding: 15px; font-size: 1.5em; line-height: 80px;');
                 console.log('\n\
       /| ________________\n\
 O|===|* >________________>\n\
@@ -12949,7 +13167,7 @@ define("Class", ["require", "exports", "EventDispatcher"], function (require, ex
     }());
     exports.Class = Class;
 });
-define("Actor", ["require", "exports", "Physics", "Class", "Collision/BoundingBox", "Resources/Texture", "Events", "Drawing/Color", "Drawing/Sprite", "Util/Log", "Actions/ActionContext", "Actions/Action", "Algebra", "Collision/Body", "Collision/Side", "Traits/Index", "Drawing/SpriteEffects", "Util/Util"], function (require, exports, Physics_13, Class_11, BoundingBox_8, Texture_2, Events_11, Color_19, Sprite_4, Log_16, ActionContext_3, Action_2, Algebra_23, Body_2, Side_5, Traits, Effects, Util) {
+define("Actor", ["require", "exports", "Physics", "Class", "Collision/BoundingBox", "Resources/Texture", "Events", "Drawing/Color", "Drawing/Sprite", "Util/Log", "Actions/ActionContext", "Actions/Action", "Algebra", "Collision/Body", "Collision/Side", "Traits/Index", "Drawing/SpriteEffects", "Util/Util"], function (require, exports, Physics_13, Class_11, BoundingBox_8, Texture_2, Events_11, Color_20, Sprite_4, Log_16, ActionContext_3, Action_2, Algebra_23, Body_2, Side_5, Traits, Effects, Util) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /**
@@ -13854,7 +14072,7 @@ define("Actor", ["require", "exports", "Physics", "Class", "Collision/BoundingBo
             // Draw actor Id
             ctx.fillText('id: ' + this.id, bb.left + 3, bb.top + 10);
             // Draw actor anchor Vector
-            ctx.fillStyle = Color_19.Color.Yellow.toString();
+            ctx.fillStyle = Color_20.Color.Yellow.toString();
             ctx.beginPath();
             ctx.arc(this.getWorldPos().x, this.getWorldPos().y, 3, 0, Math.PI * 2);
             ctx.closePath();
@@ -13866,7 +14084,7 @@ define("Actor", ["require", "exports", "Physics", "Class", "Collision/BoundingBo
                 }
             }
             // Unit Circle debug draw
-            ctx.strokeStyle = Color_19.Color.Yellow.toString();
+            ctx.strokeStyle = Color_20.Color.Yellow.toString();
             ctx.beginPath();
             var radius = Math.min(this.getWidth(), this.getHeight());
             ctx.arc(this.getWorldPos().x, this.getWorldPos().y, radius, 0, Math.PI * 2);
@@ -13880,7 +14098,7 @@ define("Actor", ["require", "exports", "Physics", "Class", "Collision/BoundingBo
             };
             var oldFont = ctx.font;
             for (var tick in ticks) {
-                ctx.fillStyle = Color_19.Color.Yellow.toString();
+                ctx.fillStyle = Color_20.Color.Yellow.toString();
                 ctx.font = '14px';
                 ctx.textAlign = 'center';
                 ctx.fillText(tick, this.getWorldPos().x + Math.cos(ticks[tick]) * (radius + 10), this.getWorldPos().y + Math.sin(ticks[tick]) * (radius + 10));

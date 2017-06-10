110251a3d8d569b75d47aed7e38da08c1bdf48fd
Travis-CI <travis@excaliburjs.com>
2017-06-10 19:51:39 +0000

    :shipit: Built excaliburjs/Excalibur@b0e4178 on branch HEAD

--- dist/excalibur.amd.js
+++ dist/excalibur.amd.js
@@ -1,4 +1,4 @@
-/*! excalibur - v0.10.0-alpha.1581+b3e5f17 - 2017-06-10
+/*! excalibur - v0.10.0-alpha.1583+ac3872f - 2017-06-10
 * https://github.com/excaliburjs/Excalibur
 * Copyright (c) 2017 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>; Licensed BSD-2-Clause
 * @preserve */
@@ -11135,7 +11135,7 @@ define("Index", ["require", "exports", "Actor", "Algebra", "Camera", "Class", "D
     /**
      * The current Excalibur version string
      */
-    exports.EX_VERSION = '0.10.0-alpha.1581+b3e5f17';
+    exports.EX_VERSION = '0.10.0-alpha.1583+ac3872f';
     // This file is used as the bundle entrypoint and exports everything
     // that will be exposed as the `ex` global variable.
     __export(Actor_10);
--- dist/excalibur.d.ts
+++ dist/excalibur.d.ts
@@ -1,4 +1,4 @@
-/*! excalibur - v0.10.0-alpha.1581+b3e5f17 - 2017-06-10
+/*! excalibur - v0.10.0-alpha.1583+ac3872f - 2017-06-10
 * https://github.com/excaliburjs/Excalibur
 * Copyright (c) 2017 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>; Licensed BSD-2-Clause
 * @preserve */

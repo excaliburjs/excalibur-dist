88df4e8c5414e81ce97a9c2a78dde48a31121255
Travis-CI <travis@excaliburjs.com>
2017-08-12 19:28:56 +0000

    :shipit: Built excaliburjs/Excalibur@13b3f9b on branch HEAD

--- dist/excalibur.amd.d.ts
+++ dist/excalibur.amd.d.ts
@@ -1,4 +1,4 @@
-/*! excalibur - v0.11.0-alpha.1688+6e32a7a - 2017-08-12
+/*! excalibur - v0.11.0-alpha.1689+13b3f9b - 2017-08-12
 * https://github.com/excaliburjs/Excalibur
 * Copyright (c) 2017 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>; Licensed BSD-2-Clause
 * @preserve */

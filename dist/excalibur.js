1c9c61b5df9947db7265d48bde0d56cb9594ace0
Travis-CI <travis@excaliburjs.com>
2018-06-27 20:12:02 +0000

    :shipit: Built excaliburjs/Excalibur@50aab71 on branch HEAD

--- dist/excalibur.js
+++ dist/excalibur.js
@@ -1,5 +1,5 @@
 /*!
- * excalibur - 0.17.0-alpha.2505+c3d02a5 - 2018-6-22
+ * excalibur - 0.17.0-alpha.2508+50aab71 - 2018-6-27
  * https://github.com/excaliburjs/Excalibur
  * Copyright (c) 2018 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>
  * Licensed BSD-2-Clause
@@ -19241,7 +19241,7 @@ __webpack_require__.r(__webpack_exports__);
  * The current Excalibur version string
  * @description `process.env.__EX_VERSION` gets replaced by Webpack on build
  */
-var EX_VERSION = "0.17.0-alpha.2505+c3d02a5";
+var EX_VERSION = "0.17.0-alpha.2508+50aab71";
 // This file is used as the bundle entrypoint and exports everything
 // that will be exposed as the `ex` global variable.
 

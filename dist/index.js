465a2fed256845da15f71c8d9e2d39fa2e64cd92
Travis-CI <travis@excaliburjs.com>
2019-04-15 22:10:41 +0000

    :shipit: Built excaliburjs/Excalibur@7e56667 on branch HEAD

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 /**
  * The current Excalibur version string
- * @description `"0.22.0-alpha.2968+7c8bdec"` gets replaced by Webpack on build
+ * @description `"0.22.0-alpha.2971+7e56667"` gets replaced by Webpack on build
  */
-export var EX_VERSION = "0.22.0-alpha.2968+7c8bdec";
+export var EX_VERSION = "0.22.0-alpha.2971+7e56667";
 // This file is used as the bundle entrypoint and exports everything
 // that will be exposed as the `ex` global variable.
 export * from './Engine';

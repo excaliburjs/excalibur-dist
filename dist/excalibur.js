87fea4a9a1cc5c275f60967f2327493b2c2b8036
Travis-CI <travis@excaliburjs.com>
2020-01-03 01:56:04 +0000

    :shipit: Built excaliburjs/Excalibur@5fd85b4 on branch HEAD

--- dist/excalibur.js
+++ dist/excalibur.js
@@ -1,7 +1,7 @@
 /*!
- * excalibur - 0.23.0-alpha.5535+31dc87c - 2019-12-28
+ * excalibur - 0.23.0-alpha.5541+5fd85b4 - 2020-1-3
  * https://github.com/excaliburjs/Excalibur
- * Copyright (c) 2019 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>
+ * Copyright (c) 2020 Excalibur.js <https://github.com/excaliburjs/Excalibur/graphs/contributors>
  * Licensed BSD-2-Clause
  * @preserve
  */
@@ -25489,7 +25489,7 @@ __webpack_require__.r(__webpack_exports__);
  * The current Excalibur version string
  * @description `process.env.__EX_VERSION` gets replaced by Webpack on build
  */
-var EX_VERSION = "0.23.0-alpha.5535+31dc87c";
+var EX_VERSION = "0.23.0-alpha.5541+5fd85b4";
 
 Object(_Polyfill__WEBPACK_IMPORTED_MODULE_0__["polyfill"])();
 // This file is used as the bundle entry point and exports everything

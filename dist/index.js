1c262208616377a69bb72d1dfb76b0d10c1dc3e9
Travis-CI <travis@excaliburjs.com>
2020-01-31 02:26:57 +0000

    :shipit: Built excaliburjs/Excalibur@17b5b1d on branch HEAD

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 /**
  * The current Excalibur version string
- * @description `"0.23.0-alpha.5718+918e5fa"` gets replaced by Webpack on build
+ * @description `"0.23.0-alpha.5723+17b5b1d"` gets replaced by Webpack on build
  */
-export var EX_VERSION = "0.23.0-alpha.5718+918e5fa";
+export var EX_VERSION = "0.23.0-alpha.5723+17b5b1d";
 import { polyfill } from './Polyfill';
 polyfill();
 // This file is used as the bundle entry point and exports everything
